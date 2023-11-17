5096e0bbd5d0ad28f7ee077b5e52e08204647fa9
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2023-11-17 23:23:35 +0000

    add TODO

--- update.R
+++ update.R
@@ -5,7 +5,7 @@ url <- "https://developer.r-project.org/blosxom.cgi/R-devel/NEWS/index.rss"
 xml <- read_xml(url) # TODO - handle failure
 latest <- xml_find_first(xml, ".//item")
 latest <- unlist(as_list(latest), recursive = FALSE)
-latest$pubDate <- anytime(latest$pubDate)
+latest$pubDate <- anytime(latest$pubDate) # TODO - handle failure
 latest$description <- xml_text(read_html(latest$description))
 
 previous <- readRDS("previous.rds")
